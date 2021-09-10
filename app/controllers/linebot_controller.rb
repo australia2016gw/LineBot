9509cd19e8765db931e45b8591dde3ce3e3fc602
Ubuntu <ubuntu@ip-172-31-24-138.us-east-2.compute.internal>
2021-09-10 09:57:20 +0000

    weather response fix

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,26 +1,15 @@
 class LinebotController < ApplicationController
-  require 'line/bot' 
- 
-  protect_from_forgery :except => [:callback]
- 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
- 
+  require 'line/bot'
+
   def callback
     body = request.body.read
- 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
     end
- 
     events = client.parse_events_from(body)
- 
-    events.each { |event|
+
+    events.each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
@@ -29,11 +18,20 @@ class LinebotController < ApplicationController
             type: 'text',
             text: event.message['text']
           }
-          client.reply_message(event['replyToken'], message)
         end
       end
-    }
- 
+      client.reply_message(event['replyToken'], message)
+    end
     head :ok
+  end
+
+private
+
+# LINE Developers登録完了後に作成される環境変数の認証
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
   end
 end
