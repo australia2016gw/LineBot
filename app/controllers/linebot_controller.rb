29af5b023f78b9e4dfd6253d7f90f466a1c72a37
Ubuntu <ubuntu@ip-172-31-24-138.us-east-2.compute.internal>
2021-09-11 07:21:57 +0000

    bug trace

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -33,9 +33,11 @@ class LinebotController < ApplicationController
           longitude = event.message['longitude']
           appId = ENV["API_KEY"]
           url= "http://api.openweathermap.org/data/2.5/forecast?lon=#{longitude}&lat=#{latitude}&APPID=#{appId}&units=metric&mode=xml"
+          p url
           # XMLをパースしていく
           xml  = open( url ).read.toutf8
           doc = REXML::Document.new(xml)
+          p doc
           xpath = 'weatherdata/forecast/time[1]/'
           nowWearther = doc.elements[xpath + 'symbol'].attributes['name']
           nowTemp = doc.elements[xpath + 'temperature'].attributes['value']
