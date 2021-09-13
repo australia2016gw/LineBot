b6de6d60789199071fd33bcf982f6818c9db0fc6
Ubuntu <ubuntu@ip-172-31-24-138.us-east-2.compute.internal>
2021-09-13 14:44:22 +0000

    comment delete

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -28,10 +28,7 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Location
-          p "here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-          p event.message['latitude']
           latitude = event.message['latitude']
-          p event.message['longitude']
           longitude = event.message['longitude']
           appId = ENV["API_KEY"]
           url= "http://api.openweathermap.org/data/2.5/forecast?lon=#{longitude}&lat=#{latitude}&APPID=#{appId}&units=metric&mode=xml"
@@ -41,36 +38,27 @@ class LinebotController < ApplicationController
           xpath = 'weatherdata/forecast/time[1]/'
           nowWearther = doc.elements[xpath + 'symbol'].attributes['name']
           nowTemp = doc.elements[xpath + 'temperature'].attributes['value']
-          p nowWearther
           case nowWearther
           # 条件が一致した場合、メッセージを返す処理。絵文字も入れています。
           when /.*(clear sky|few clouds).*/
             push = "現在地の天気は晴れです\u{2600}\n\n現在の気温は#{nowTemp}℃です\u{1F321}"
-            p "晴れ"
           when /.*(scattered clouds|broken clouds|overcast clouds).*/
             push = "現在地の天気は曇りです\u{2601}\n\n現在の気温は#{nowTemp}℃です\u{1F321}"
-            p "曇り"
           when /.*(rain|thunderstorm|drizzle).*/
             push = "現在地の天気は雨です\u{2614}\n\n現在の気温は#{nowTemp}℃です\u{1F321}"
-            p "雨"
           when /.*(snow).*/
             push = "現在地の天気は雪です\u{2744}\n\n現在の気温は#{nowTemp}℃です\u{1F321}"
-            p "雪"
           when /.*(fog|mist|Haze).*/
             push = "現在地では霧が発生しています\u{1F32B}\n\n現在の気温は#{nowTemp}℃です\u{1F321}"
-            p "霧"
           else
             push = "現在地では何かが発生していますが、\nご自身でお確かめください。\u{1F605}\n\n現在の気温は#{nowTemp}℃です\u{1F321}"
-            p "その他"
           end
-          p push
           message = {
             type: 'text',
             text: push
           }
           client.reply_message(event['replyToken'], message)
         end
-        p "end???????????????????????????????????????????????"
         head :ok
       end
     end
